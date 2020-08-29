314ddeb8af904a6df9488e8c6fc6bdec2f06cb2e
Juan Bollati <jbollati@users.noreply.github.com>
2020-08-28 23:43:38 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-
+from sklearn.utils import resample
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class CSTransformer(BaseEstimator, TransformerMixin):
@@ -12,6 +12,13 @@ class CSTransformer(BaseEstimator, TransformerMixin):
     def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
+        
+        df_majority = data[data.OBJETIVO=='Aceptado']
+        df_minority = data[data.OBJETIVO=='Sospechoso']
+
+        df_minority_upsampled = resample(df_minority, replace=True, n_samples = 8873, random_state=123)
+
+        data = pd.concat([df_majority, df_minority_upsampled])
         data = data.drop(labels=self.columns, axis='columns')
         data = data.fillna(data.mean())
         # Devolvemos un nuevo dataframe de datos preprocesados
